3ff9f9a363c9c968d4c3cb6014e88dc3e8e8b725
Khanbala Rashidov <50279392+KhanbalaRashidov@users.noreply.github.com>
2024-08-01 20:22:16 +0400

    Added stringsandrunes.go

---
+++ stringsandrunes.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+
+	const s = "necəsən?"
+
+	fmt.Println(len(s))
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%x", s[i])
+	}
+	fmt.Println()
+	fmt.Println("Rune count:", utf8.RuneCountInString(s))
+
+	for idx, runeValue := range s {
+		fmt.Printf("%#U starts at %d\n", runeValue, idx)
+	}
+	fmt.Println("\nUsing DecodeRuneInString")
+	for i, w := 0, 0; i < len(s); i += w {
+		runeValue, width := utf8.DecodeRuneInString(s[i:])
+		fmt.Printf("%#U starts at %d\n", runeValue, i)
+		w = width
+	}
+}
